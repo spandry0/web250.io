c96233a33a292cdb42faab3042bf7a657432abce
STEPHANE PANDRY <122483914+spandry0@users.noreply.github.com>
2024-05-07 23:11:26 -0400

    contract.php

---
+++ contents/contract.php
@@ -0,0 +1,12 @@
+
+ <h2>Contract</h2>
+        <p>
+           I, Stephane Pandry agree to abide by the terms of the course contract in my SEMESTER 2024, Web 250 with my instructor, Professor D.I. von Briesen. (Note - Don’t use the caps- but replace those with your data, with normal letter casing)
+      
+            I understand that all work that I do on publicly available online tools will be available to the world, and will not put information there that is inappropriate for schoolwork, or that I wish to keep private.
+      
+           I also understand that it is my work that counts for attendance, not logins or showing up for class. As such, failure to turn in assignments may show as absences.
+	</p>
+     </main>
+      
+            <br>Signed: Stephane Pandry, 02/22/2024</br></p>
