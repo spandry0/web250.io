0104124a1ef9bb2de5767b6c382343ed16cc4b0c
STEPHANE PANDRY <122483914+spandry0@users.noreply.github.com>
2024-05-07 23:14:02 -0400

    introduction.php

---
+++ contents/introduction.php
@@ -0,0 +1,23 @@
+
+ <h2>Introduction</h2>
+          <div class="introImage">
+                  <img src="images/Pandry.jpeg" alt="Pandry" width="300" height="400">
+		
+   
+         </div>
+           <main class="introMain">
+        <div class="introList">
+            <ul>
+                <li><b>Personal Background:</b> I grew up in Ivory Coast (West Africa). I have lived in the US for 7 years. I got my high school diploma and I graduated in electromechanical engineering in Ivory Coast. My first language is French and I learned English at CPCC.</li>
+                <li><b>Professional Background:</b> I worked at the airport  and I work at Amazon as a Problem solver.</li>
+                <li><b>Academic Background:</b>I’ve got a certificate in information technology. I will get my associate degree this year in Database development/Business intelligence.</li>
+                <li><b>Background in this Subject:</b> I have been using computers since I was in high school. I have some knowledge of programming.</li>
+                <li><b>Primary Computer Platform:</b> Windows 10 Home and office.</li>
+                <li><b>Courses I'm Taking & Why:</b>
+			
+                <li><b>Web 250-Database Driven Website</b>: Database Driven; Websites substitute course for CTI 289.</li>
+                <li><b>CTI 130-N882 Os and Device Foundation</b>: I am retaking this course because I didn’t like my final grade.</li>  
+                <li><b>Funny/Interesting Item to Remember me by</b>: People seeing me think that I am mean but they notice that they are wrong when they speak to me.!</li> 
+                <li><b>I'd also like to share </b>: I enjoy spending time outside with my son walking, playing, and any number of other activities that keep him off the screens.</li>
+            </ul>
+      
