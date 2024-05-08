bfdd92449975f2bc851b71b69a2fb62ccc5b48c5
STEPHANE PANDRY <122483914+spandry0@users.noreply.github.com>
2024-05-07 23:14:48 -0400

    brand.php

---
+++ contents/brand.php
@@ -0,0 +1,26 @@
+
+      <h2>Brand</h2>
+	<h2>Concept</h2>
+           <p><i>" Talent. Genius. Creativity. "</i></p>
+            <h2>Pyramid</h2>
+	      
+      <div class="mainPyramid">
+	      
+        <p> Pyramid idea is to help children develop their talents: Expose them to new activities; Involve them in daily Activities; and Provide opportunities for deliberate practice.</p>
+         <br>
+
+        <div class="color">
+          <p class="brandP">Colors</p>
+          <div class="colorsBox">
+            <div class="color1">
+              <p>#0088dd</p>
+            </div>
+            <div class="color2">
+              <p>#413f3b</p>
+            </div>
+            <div class="color3">
+              <p>#66cccc</p>
+            </div>
+            <div class="color4">
+              <p>#efefef</p>
+             </div>
